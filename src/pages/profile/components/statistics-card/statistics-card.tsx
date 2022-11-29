2c7bce5f5d02460f33bd2ec73f9166b85dfd165a
Muaz Kholho <mikimaus-777@mail.ru>
2022-11-29 19:50:04 +0300

    feature-8: Компонент StatisticsEvent сверстан.

--- src/pages/profile/components/statistics-card/statistics-card.tsx
+++ src/pages/profile/components/statistics-card/statistics-card.tsx
@@ -1,8 +1,28 @@
 import { Card } from 'components/card';
+import LocationImage from 'assets/images/location.svg';
+import AcceptImage from 'assets/images/accept.svg';
 import styles from './statistics-card.module.scss';
 
 function StatisticsEvent() {
-  return <li className={styles.statisticsCardEvent}></li>;
+  return (
+    <li className={styles.statisticsCardEvent}>
+      <div className={styles.statisticsCardEventDate}>
+        <p className={styles.statisticsCardEventDateText}>окт</p>
+        <p className={styles.statisticsCardEventDateNumber}>19</p>
+      </div>
+      <div className={styles.statisticsCardEventInfo}>
+        <h3 className={styles.statisticsCardEventInfoTitle}>День Дня</h3>
+        <p className={styles.statisticsCardEventInfoLocation}>
+          <LocationImage className={styles.statisticsCardEventInfoLocationIcon} />
+          Магас, главный корпус
+        </p>
+      </div>
+      <div className={styles.statisticsCardEventAccept}>
+        <AcceptImage />
+        <p className={styles.statisticsCardEventAcceptText}>Выполнено</p>
+      </div>
+    </li>
+  );
 }
 
 export function StatisticsCard() {
